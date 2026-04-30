55882324442f1ca895ca5304df407f9be7650c58
bbartje <52001218+bbartje@users.noreply.github.com>
2026-04-30 22:35:23 +0200

    Update __init__.py

--- custom_components/ctek_ble/__init__.py
+++ custom_components/ctek_ble/__init__.py
@@ -1,1 +1,14 @@
-DOMAIN = "ctek_ble"
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
